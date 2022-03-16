f3539602e4a6744c61eb97d29b5aaa9b09bf542c
yaofeiran <feiran.21@intl.zju.edu.cn>
2022-03-16 21:47:00 +0800

    variable

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -5,4 +5,17 @@ d=b-c
 print("2021-2020 difference",d)
 e=a-b
 print("2022-2021 difference",e)
-
+if d==e:
+ print("d=e")
+elif d>e:
+ print("d>e")
+else:
+ print("d<e")
+f=d/c
+g=e/b
+if f==g:
+ print("f==g")
+elif f>g:
+ print("f>g")
+else:
+ print("f<g")
