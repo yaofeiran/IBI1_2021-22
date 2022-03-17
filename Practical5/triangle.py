6d0a4c955cea227eb1d465ac2d4d585eca2f25f9
yaofeiran <feiran.21@intl.zju.edu.cn>
2022-03-17 21:12:18 +0800

    edit

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,11 +1,16 @@
-#Repeat,set the last sum as x, the number of triangle as n,sum of the current triangle as y
+#Repeat 10 times
+#y=1+2+3+...+n-1+n
+#for (n-1)th triangle,y=x
+#then for nth triangle,y=x+n
+#set the last sum as x, the number of triangle as n,sum of the current triangle as y
+#start as n=1,x=0
+#end when n=10 
 x=0
 for n in range(1,11):
-#x is the bridge
   y=x+n
 #x becomes y,which is the last sum
   x=y
-#Done!
+#repeat
   print(n,y)
   
  
--- Practical5/variable.py
+++ Practical5/variable.py
@@ -2,20 +2,66 @@ a=19245301
 b=4218520
 c=271
 d=b-c
-print("2021-2020 difference",d)
+print("2021-2020 difference d",d)
 e=a-b
-print("2022-2021 difference",e)
+print("2022-2021 difference e",e)
 if d==e:
  print("d=e")
 elif d>e:
  print("d>e")
 else:
  print("d<e")
+#f is the rate of new cases in 2020, g is the rate of the cases in 2021
 f=d/c
 g=e/b
 if f==g:
- print("f==g")
+ print("the rate of new cases is the same")
 elif f>g:
- print("f>g")
+ print("2020 has the greater rate of new cases")
 else:
- print("f<g")
+ print("2021 has the greater rate of new cases")
+
+X="111"
+Y="222"
+W=X and Y
+print(W)
+
+X="0"
+Y="10000"
+W=X and Y
+print(W)
+
+X=3
+Y="string"
+W=X and Y
+print(W)
+
+X=True
+Y=False
+W=X and Y
+print(W)
+
+X=False
+Y=True
+W=X and Y
+print(W)
+
+X="False"
+Y="True"
+W=X and Y
+print(W)
+
+X=0
+Y=111
+W=X and Y
+print(W)
+
+X=1
+Y=111
+W=X and Y
+print(W)
+
+X=0
+Y="string"
+W=X and Y
+print(W)
