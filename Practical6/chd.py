c49a9340d0590e2e7a4b3c0278e276453650b3e4
yaofeiran <feiran.21@intl.zju.edu.cn>
2022-03-22 16:34:05 +0800

    edit

--- Practical6/chd.py
+++ Practical6/chd.py
@@ -1,5 +1,8 @@
-chd={'30':1.03,'35':1.07,'40':1.11,'45':1.17,'50':1.23,'55':1.32,'60':1.42,'65':1.55,'70':1.72,'75':1.94}
-print(chd)
+paternal_age=[30,35,40,45,50,55,60,65,70,75]
+chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
+D=dict(zip(paternal_age, chd))#D is the dictionary
+print(D)
+
 import numpy as np
 import matplotlib.pyplot as plt
 N=10
@@ -10,3 +13,6 @@ plt.title('Paternal age on offspring health')
 plt.xlabel('paternal age')
 plt.ylabel('chd')
 plt.show()
+
+age=30#age is the variable pf the requested paternal age that can be modified
+print(D[age])
